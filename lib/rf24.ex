b0bda6b785f561de291083785fcd83fcb1541fe6
Connor Rigby <konnorrigby@gmail.com>
2020-05-29 18:03:45 -0700

    Small refactor and cleanup

--- lib/rf24.ex
+++ lib/rf24.ex
@@ -1,6 +1,14 @@
 defmodule RF24 do
   import RF24.Util
 
+  # defmodule Address do
+  #   defstruct [
+  #     auto_ack?: false,
+  #     index: 0,
+  #     address: <<255,0xFC,0xE1,0xA8,0xA8>>
+  #   ]
+  # end
+
   defstruct csn: nil,
             ce: nil,
             irq: nil,
@@ -8,14 +16,18 @@ defmodule RF24 do
             rx: nil,
             spi: nil,
             ce_pin: 87,
-            # csn_pin: 5,
             csn_pin: 23,
             irq_pin: 89,
             spi_bus_name: "spidev1.0",
-            p_variant: false,
-            dynamic_payloads_enabled: nil,
-            payload_size: 32,
-            pipe0_reading_address: nil
+            channel: 76,
+            crc?: true,
+            crc_2_bit?: true,
+            auto_retransmit_delay: 5,
+            auto_retransmit_count: 15,
+            data_rate: :RF24_1MBPS
+
+  # data_rate: :RF24_2MBPS
+  # data_rate: :RF24_250KBPS
 
   use GenServer
 
@@ -23,19 +35,34 @@ defmodule RF24 do
     GenServer.start_link(__MODULE__, args, opts)
   end
 
-  def init(args) do
-    send(self(), :init)
-    {:ok, struct(RF24, args)}
+  def open_reading_pipe(pid, child, addr)
+      when byte_size(addr) in [3, 4, 5] and
+             child in [
+               :RX_ADDR_P0,
+               :RX_ADDR_P1,
+               :RX_ADDR_P2,
+               :RX_ADDR_P3,
+               :RX_ADDR_P4,
+               :RX_ADDR_P5
+             ] do
+  end
+
+  def open_writing_pipe(pid, addr) when byte_size(addr) in [3, 4, 5] do
   end
 
   def radio_init(rf24) do
     with {:ok, ce} <- gpio_open(rf24.ce_pin, :output, initial_value: 0),
          {:ok, csn} <- gpio_open(rf24.csn_pin, :output, initial_value: 1),
          {:ok, irq} <- gpio_open(rf24.irq_pin, :input),
-         :ok <- gpio_set_interrupts(irq, :both),
+         :ok <- gpio_set_interrupts(irq, :falling),
          {:ok, spi} <- spi_open(rf24.spi_bus_name, mode: 0, speed_hz: 10_000_000) do
       %{rf24 | ce: ce, csn: csn, irq: irq, spi: spi}
     end
+  end
+
+  def init(args) do
+    send(self(), :init)
+    {:ok, struct(RF24, args)}
   end
 
   def handle_info(:init, rf24) do
@@ -49,35 +76,37 @@ defmodule RF24 do
   end
 
   def handle_info(:reset, rf24) do
-    # gpio_write(rf24.ce, 1)
     gpio_write(rf24.ce, 0)
     gpio_write(rf24.csn, 1)
 
     Process.sleep(50)
-    rf24 = write_reg(rf24, :NRF_CONFIG, 0x0C)
 
     rf24 =
       rf24
-      |> set_retries(5, 15)
-      |> set_data_rate(:RF24_250KBPS)
-
-    setup = read_reg(rf24, :RF_SETUP)
-
-    rf24 =
-      rf24
-      |> set_data_rate(:RF24_1MBPS)
-      |> toggle_features()
-      |> write_reg(:FEATURE, 0)
-      |> write_reg(:DYNPD, 0)
-      |> Map.put(:dynamic_payloads_enabled, false)
-      |> write_reg(:NRF_STATUS, <<0b01110000::8>>)
-      |> set_channel(76)
+      # |> write_reg(:FEATURE, 0)
+      # |> write_reg(:DYNPD, 0)
+      # |> write_reg(:EN_AA, 0)
+      |> write_reg(:FEATURE, 0b00000111)
+      |> write_reg(:DYNPD, 0b00111111)
+      |> write_reg(:EN_AA, 0b00111111)
+      |> write_reg(:EN_RXADDR, 0)
+      |> write_reg(:RX_ADDR_P0, <<0xE7, 0xE7, 0xE7, 0xE7, 0xE7>>)
+      |> write_reg(:RX_ADDR_P1, <<0xC2, 0xC2, 0xC2, 0xC2, 0xC2>>)
+      |> write_reg(:RX_ADDR_P2, 0xC3)
+      |> write_reg(:RX_ADDR_P3, 0xC4)
+      |> write_reg(:RX_ADDR_P4, 0xC5)
+      |> write_reg(:RX_ADDR_P5, 0xC6)
+      |> write_reg(:TX_ADDR, <<0xE7, 0xE7, 0xE7, 0xE7, 0xE7>>)
+      |> set_crc()
+      |> set_retries()
+      |> set_data_rate()
+      |> set_channel()
       |> flush_rx()
       |> flush_tx()
       |> power_up()
       |> enable_ptx()
-      |> enable_ack_payload()
-      |> enable_dynamic_payloads()
+
+    setup = read_reg(rf24, :RF_SETUP)
 
     if setup != 0x0 && setup != 0xFF do
       send(self(), :start_listening)
@@ -90,52 +119,32 @@ defmodule RF24 do
   def handle_info(:start_listening, rf24) do
     rf24 =
       rf24
-      |> open_writing_pipe(<<0xCE, 0xCC, 0xCE, 0xCC, 0xCE>>)
-      |> open_reading_pipe(:RX_ADDR_P1, <<0xCC, 0xCE, 0xCC, 0xCE, 0xCC>>)
+      |> write_reg(:EN_RXADDR, <<0b00111111::8>>)
       |> power_up()
       |> enable_prx()
 
     gpio_write(rf24.ce, 1)
-
-    # Restore the pipe0 adddress, if exists
-    rf24 =
-      if rf24.pipe0_reading_address do
-        write_reg(rf24, :RX_ADDR_P0, rf24.pipe0_reading_address)
-      else
-        close_reading_pipe(rf24, :RX_ADDR_P0)
-      end
-
-    case read_reg_bin(rf24, :FEATURE) do
-      # check for en_ack_pay being set
-      <<_::5, _en_dpl::1, 1::1, _en_dy_ack::1>> ->
-        rf24 = flush_tx(rf24)
-        {:noreply, rf24}
-
-      _ ->
-        {:noreply, rf24}
-    end
-
     {:noreply, rf24}
   end
 
-  def handle_info({:circuits_gpio, _pin, _ts, _} = interupt, rf24) do
-    IO.puts("interupt: #{inspect(interupt)}")
-    %{rx_dr: rx} = status = read_status(rf24)
-    IO.inspect(status, label: "status")
+  def handle_info({:circuits_gpio, _pin, _ts, _}, rf24) do
+    IO.puts("interupt")
+    rf24 =
+      case read_reg_bin(rf24, :NRF_STATUS) do
+        <<_::1, 1::1, tx::1, max_retry::1, pipe::3, tx_full::1>> ->
+          write_reg(rf24, :NRF_STATUS, <<0::1, 1::1, 1::1, 1::1, pipe::3, tx_full::1>>)
+          length = read_payload_length(rf24)
+          payload = read_payload(rf24, length)
 
-    # if tx do
-    # end
+          IO.inspect(length, label: "length")
+          IO.inspect(payload, label: "PAYLOAD from pipe: #{pipe}")
 
-    # if fail do
-    # end
+          rf24
+        unk ->
+          IO.inspect(unk, label: "unknown")
+          rf24
+      end
 
-    if rx do
-      payload = read_payload(rf24)
-      IO.inspect(payload, label: "PAYLOAD")
-      rf24 = reset_status(rf24)
-      {:noreply, rf24}
-    else
-      {:noreply, rf24}
-    end
+    {:noreply, rf24}
   end
 end
--- lib/rf24/util.ex
+++ lib/rf24/util.ex
@@ -39,97 +39,35 @@ defmodule RF24.Util do
   end
 
   def write_reg(rf24, addr, value) when addr <= 31 and is_binary(value) do
-    # IO.puts("WRITE_REG #{inspect(addr, base: :hex)} #{inspect(value, base: :hex)}")
     rf24 = select(rf24)
-    # {:ok, status} = spi_transfer(rf24.spi, <<0b001::3, addr::5>>)
-    # {:ok, return} = spi_transfer(rf24.spi, value)
-
-    # status = _SPI.transfer(W_REGISTER | (REGISTER_MASK & reg));
-    reg = instr(:W_REGISTER) ||| (instr(:REGISTER_MASK) &&& addr)
-    {:ok, status} = spi_transfer(rf24.spi, <<reg::8>>)
-    {:ok, return} = spi_transfer(rf24.spi, value)
-    IO.inspect(status, label: "[#{inspect(addr, base: :hex)}] WRITE STATUS")
-    IO.inspect(return, label: "[#{inspect(addr, base: :hex)}] WRITE RETURN")
+    {:ok, _status} = spi_transfer(rf24.spi, <<0b001::3, addr::5>>)
+    {:ok, _return} = spi_transfer(rf24.spi, value)
     unselect(rf24)
   end
 
-  def enable_ack_payload(rf24) do
-    <<_::5, _en_dpl::1, _en_ack_pay::1, en_dy_ack::1>> = read_reg_bin(rf24, :FEATURE)
+  # def open_writing_pipe(rf24, address) when byte_size(address) in [3, 4, 5] do
+  #   Logger.info("Opening writing pipe to #{inspect(address, base: :hex)}")
 
-    rf24
-    |> write_reg(:FEATURE, <<0::5, 1::1, 1::1, en_dy_ack::1>>)
-    |> write_reg(:DYNPD, <<0::2, 0b111111::6>>)
-  end
+  #   rf24
+  #   |> set_address_width(byte_size(address))
+  #   |> write_reg(:RX_ADDR_P0, address)
+  #   |> write_reg(:TX_ADDR, address)
+  #   |> write_reg(:RX_PW_P0, rf24.payload_size)
+  # end
 
-  def enable_dynamic_payloads(rf24) do
-    <<_::5, _en_dpl::1, en_ack_pay::1, en_dy_ack::1>> = read_reg_bin(rf24, :FEATURE)
+  # def open_reading_pipe(rf24, child, address)
+  #     when child in [:RX_ADDR_P0, :RX_ADDR_P1, :RX_ADDR_P2, :RX_ADDR_P3, :RX_ADDR_P4, :RX_ADDR_P5] do
+  #   Logger.info(
+  #     "Opening reading pipe #{inspect(child)} with address: #{inspect(address, base: :hex)}"
+  #   )
 
-    rf24
-    |> write_reg(:FEATURE, <<0::5, 1::1, en_ack_pay::1, en_dy_ack::1>>)
-    |> write_reg(:DYNPD, <<0::2, 0b111111::6>>)
-  end
-
-  def open_writing_pipe(rf24, address) when byte_size(address) in [3, 4, 5] do
-    Logger.info("Opening writing pipe to #{inspect(address, base: :hex)}")
-
-    rf24
-    |> set_address_width(byte_size(address))
-    |> write_reg(:RX_ADDR_P0, address)
-    |> write_reg(:TX_ADDR, address)
-    |> write_reg(:RX_PW_P0, rf24.payload_size)
-  end
-
-  def close_reading_pipe(rf24, child)
-      when child in [:RX_ADDR_P0, :RX_ADDR_P1, :RX_ADDR_P2, :RX_ADDR_P3, :RX_ADDR_P4, :RX_ADDR_P5] do
-    Logger.info("Closing writing pipe #{inspect(child)}")
-    rf24
-  end
-
-  def open_reading_pipe(rf24, child, address)
-      when child in [:RX_ADDR_P0, :RX_ADDR_P1, :RX_ADDR_P2, :RX_ADDR_P3, :RX_ADDR_P4, :RX_ADDR_P5] do
-    Logger.info(
-      "Opening reading pipe #{inspect(child)} with address: #{inspect(address, base: :hex)}"
-    )
-
-    # // If this is pipe 0, cache the address.  This is needed because
-    # // openWritingPipe() will overwrite the pipe 0 address, so
-    # // startListening() will have to restore it.
-    rf24 =
-      if child == :RX_ADDR_P0 do
-        %{rf24 | pipe0_reading_address: address}
-      else
-        rf24
-      end
-
-    rf24
-    |> write_reg(child, address)
-    # :RX_ADDR_PX => RX_PW_PX are 7 addresses appart.
-    |> write_reg(reg(child) + 7, rf24.payload_size)
-    # enable RX on all pipes
-    |> write_reg(:EN_RXADDR, <<0b00111111::8>>)
-
-    # case read_reg(rf24, :EN_RXADDR) do
-    # end
-
-    # if child < 2 do
-    #   # write_register(pgm_read_byte(&child_pipe[child]), reinterpret_cast<const uint8_t*>(&address), addr_width);
-    #   write_reg()
-    # else
-
-    # end
-  end
-
-  def set_address_width(rf24, 3) do
-    write_reg(rf24, :SETUP_AW, <<0::6, 0b01::2>>)
-  end
-
-  def set_address_width(rf24, 4) do
-    write_reg(rf24, :SETUP_AW, <<0::6, 0b10::2>>)
-  end
-
-  def set_address_width(rf24, 5) do
-    write_reg(rf24, :SETUP_AW, <<0::6, 0b11::2>>)
-  end
+  #   rf24
+  #   |> write_reg(child, address)
+  #   # :RX_ADDR_PX => RX_PW_PX are 7 addresses appart.
+  #   |> write_reg(reg(child) + 7, rf24.payload_size)
+  #   # enable RX on all pipes
+  #   |> write_reg(:EN_RXADDR, <<0b00111111::8>>)
+  # end
 
   def read_reg(rf24, addr) do
     <<value>> = read_reg_bin(rf24, addr)
@@ -143,18 +81,8 @@ defmodule RF24.Util do
 
   def read_reg_bin(rf24, addr) when addr <= 31 do
     rf24 = select(rf24)
-    # {:ok, status} = spi_transfer(rf24.spi, <<0b000::3, addr::5>>)
-    # {:ok, value} = spi_transfer(rf24.spi, <<0xff::8>>)
-
-    # _SPI.transfer(R_REGISTER | (REGISTER_MASK & reg));
-    # result = _SPI.transfer(0xff);
-    reg = instr(:R_REGISTER) ||| (instr(:REGISTER_MASK) &&& addr)
-    {:ok, status} = spi_transfer(rf24.spi, <<reg::8>>)
+    {:ok, _status} = spi_transfer(rf24.spi, <<0b000::3, addr::5>>)
     {:ok, value} = spi_transfer(rf24.spi, <<0xFF::8>>)
-
-    IO.inspect(status, label: "[#{inspect(addr, base: :hex)}] READ STATUS")
-    IO.inspect(value, label: "[#{inspect(addr, base: :hex)}] READ VALUE")
-
     unselect(rf24)
     value
   end
@@ -172,10 +100,29 @@ defmodule RF24.Util do
     }
   end
 
-  def read_payload(rf24) do
+  def set_address_width(rf24, 3) do
+    write_reg(rf24, :SETUP_AW, <<0::6, 0b01::2>>)
+  end
+
+  def set_address_width(rf24, 4) do
+    write_reg(rf24, :SETUP_AW, <<0::6, 0b10::2>>)
+  end
+
+  def set_address_width(rf24, 5) do
+    write_reg(rf24, :SETUP_AW, <<0::6, 0b11::2>>)
+  end
+
+  def read_payload_length(rf24) do
+    select(rf24)
+    {:ok, <<_, length>>} = spi_transfer(rf24.spi, <<0b01100000, 0xFF>>)
+    unselect(rf24)
+    length
+  end
+
+  def read_payload(rf24, length) do
     rf24 = select(rf24)
     {:ok, _status} = spi_transfer(rf24.spi, <<instr(:R_RX_PAYLOAD)>>)
-    {:ok, payload} = spi_transfer(rf24.spi, <<0xFF::32>>)
+    {:ok, payload} = spi_transfer(rf24.spi, :binary.copy(<<0xFF>>, length))
     unselect(rf24)
     payload
   end
@@ -188,58 +135,82 @@ defmodule RF24.Util do
     write_reg(rf24, :NRF_STATUS, <<0::1, 1::1, 1::1, 1::1, pipe::3, tx_full::1>>)
   end
 
+  def set_retries(%{} = rf24) do
+    set_retries(rf24, rf24.auto_retransmit_delay, rf24.auto_retransmit_count)
+  end
+
   def set_retries(rf24, delay, count) when delay <= 15 and count <= 15 do
     # write_reg(SETUP_RETR, (delay & 0xf) << ARD | (count & 0xf) << ARC);
-    write_reg(rf24, :SETUP_RETR, <<delay::4, count::4>>)
+    write_reg(
+      %{rf24 | auto_retransmit_delay: delay, auto_retransmit_count: count},
+      :SETUP_RETR,
+      <<delay::4, count::4>>
+    )
+  end
+
+  def set_channel(rf24) do
+    set_channel(rf24, rf24.channel)
   end
 
   def set_channel(rf24, channel) when channel <= 125 do
-    write_reg(rf24, :RF_CH, channel)
+    write_reg(%{rf24 | channel: channel}, :RF_CH, channel)
   end
 
-  def get_channel(rf24) do
-    read_reg(rf24, :RF_CH)
+  def set_data_rate(rf24) do
+    set_data_rate(rf24, rf24.data_rate)
   end
 
-  def set_data_rate(rf24, data_rate) do
+  def set_data_rate(rf24, data_rate)
+      when data_rate in [:RF24_250KBPS, :RF24_1MBPS, :RF24_2MBPS] do
     <<cont_wave::1, _::1, _rf_dr_low::1, pll_lock::1, _rf_dr_high::1, rf_pwr::2, _::1>> =
       read_reg_bin(rf24, :RF_SETUP)
 
     case data_rate do
       :RF24_250KBPS ->
         value = <<cont_wave::1, 0::1, 1::1, pll_lock::1, 0::1, rf_pwr::2, 0::1>>
-        write_reg(%{rf24 | p_variant: true}, :RF_SETUP, value)
+        write_reg(%{rf24 | data_rate: :RF24_250KBPS}, :RF_SETUP, value)
 
       :RF24_2MBPS ->
         value = <<cont_wave::1, 0::1, 0::1, pll_lock::1, 1::1, rf_pwr::2, 0::1>>
-        write_reg(rf24, :RF_SETUP, value)
+        write_reg(%{rf24 | data_rate: :RF24_2MBPS}, :RF_SETUP, value)
 
       :RF24_1MBPS ->
         value = <<cont_wave::1, 0::1, 0::1, pll_lock::1, 0::1, rf_pwr::2, 0::1>>
-        write_reg(rf24, :RF_SETUP, value)
+        write_reg(%{rf24 | data_rate: :RF24_1MBPS}, :RF_SETUP, value)
     end
   end
 
-  def enable_ptx(rf24) do
-    <<_::1, mask_rx_dr::1, mask_tx_ds::1, mask_max_rt::1, en_crc::1, crco::1, pwr_up::1,
-      _prim_rx::1>> = read_reg_bin(rf24, :NRF_CONFIG)
-
-    write_reg(
-      rf24,
-      :NRF_CONFIG,
-      <<0::1, mask_rx_dr::1, mask_tx_ds::1, mask_max_rt::1, en_crc::1, crco::1, pwr_up::1, 0::1>>
-    )
+  @doc "Configures crc enabled and crc encoding"
+  def set_crc(rf24) do
+    <<_::4, _en_crc::1, _crco::1, pwr_up::1, prim_rx::1>> = read_reg_bin(rf24, :NRF_CONFIG)
+    crc? = if rf24.crc?, do: 1, else: 0
+    crc_2_bit? = if rf24.crc_2_bit?, do: 1, else: 0
+    write_reg(rf24, :NRF_CONFIG, <<0::4, crc?::1, crc_2_bit?::1, pwr_up::1, prim_rx::1>>)
   end
 
-  def enable_prx(rf24) do
-    <<_::1, mask_rx_dr::1, mask_tx_ds::1, mask_max_rt::1, en_crc::1, crco::1, pwr_up::1,
-      _prim_rx::1>> = read_reg_bin(rf24, :NRF_CONFIG)
+  # unset bit 0 on NRF_CONFIG
+  def enable_ptx(rf24) do
+    <<head::7, _prim_rx::1>> = read_reg_bin(rf24, :NRF_CONFIG)
+    write_reg(rf24, :NRF_CONFIG, <<head::7, 0::1>>)
+  end
 
-    write_reg(
-      rf24,
-      :NRF_CONFIG,
-      <<0::1, mask_rx_dr::1, mask_tx_ds::1, mask_max_rt::1, en_crc::1, crco::1, pwr_up::1, 1::1>>
-    )
+  # set bit 0 on NRF_CONFIG
+  def enable_prx(rf24) do
+    <<head::7, _prim_rx::1>> = read_reg_bin(rf24, :NRF_CONFIG)
+    write_reg(rf24, :NRF_CONFIG, <<head::7, 1::1>>)
+  end
+
+  # unset bit 1 on NRF_CONFIG if it is high
+  def power_up(rf24) do
+    case read_reg_bin(rf24, :NRF_CONFIG) do
+      # bit 1 is high
+      <<head::6, 0::1, prim_rx::1>> ->
+        write_reg(rf24, :NRF_CONFIG, <<head::6, 1::1, prim_rx::1>>)
+
+      # bit 1 is already low
+      <<_head::6, 1::1, _prim_rx::1>> ->
+        rf24
+    end
   end
 
   def toggle_features(rf24) do
@@ -258,23 +229,6 @@ defmodule RF24.Util do
     rf24 = select(rf24)
     {:ok, _} = spi_transfer(rf24.spi, <<instr(:FLUSH_TX)>>)
     unselect(rf24)
-  end
-
-  def power_up(rf24) do
-    case read_reg_bin(rf24, :NRF_CONFIG) do
-      # bit 1 is high
-      <<_::1, mask_rx_dr::1, mask_tx_ds::1, mask_max_rt::1, en_crc::1, crco::1, 0::1, prim_rx::1>> ->
-        write_reg(
-          rf24,
-          :NRF_CONFIG,
-          <<0::1, mask_rx_dr::1, mask_tx_ds::1, mask_max_rt::1, en_crc::1, crco::1, 1::1,
-            prim_rx::1>>
-        )
-
-      <<_::1, _mask_rx_dr::1, _mask_tx_ds::1, _mask_max_rt::1, _en_crc::1, _crco::1, 1::1,
-        _prim_rx::1>> ->
-        rf24
-    end
   end
 
   def select(rf24) do
